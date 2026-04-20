cefe86276db5859307a4a33760735483410af63e
flashscapes <alvinziegler@gmail.com>
2026-04-20 15:34:26 -0700

    Add text-to-speech feature in xiSpeak function
    Implement text-to-speech functionality using fetch to retrieve audio.

--- app1.js
+++ app1.js
@@ -308,8 +308,34 @@ var _speaking = false;
 
 function xiSpeak(text, onStart, onEnd) {
   xiStop();
-  browserSpeak(text, onStart, onEnd);
+  if (onStart) onStart();
+  fetch('/api/speak', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text: text })
+  })
+  .then(function(r) { return r.blob(); })
+  .then(function(blob) {
+    var url = URL.createObjectURL(blob);
+    var audio = new Audio(url);
+    _speaking = true;
+    audio.onended = function() {
+      _speaking = false;
+      URL.revokeObjectURL(url);
+      if (onEnd) onEnd();
+    };
+    audio.onerror = function() {
+      _speaking = false;
+      if (onEnd) onEnd();
+      browserSpeak(text, null, onEnd);
+    };
+    audio.play();
+  })
+  .catch(function() {
+    browserSpeak(text, null, onEnd);
+  });
   return true;
+}
 }
 
 function xiStop() {
